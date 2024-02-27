1b03b903e60f8c49381d6027f64662ea65270292
Alexia A-J <alexia_allard-jacquin@hotmail.fr>
2024-02-27 10:47:09 +0100

    Add routing logic

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,22 @@
+import {Routes, Route } from 'react-router-dom'
+import Header from './components/Header'
+import Home from './pages/Home'
+import About from './pages/About'
+import FicheLogement from './pages/FicheLogement'
+import Error from './pages/Error'
+import Footer from './components/Footer'
 
 function App() {
   return (
     <div>
-      Hello
+      <Header />
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/about' element={<About />} />
+        <Route path='/logement/id' element={<FicheLogement />} />
+        <Route path='*' element={<Error />} />
+      </Routes>
+      <Footer />
     </div>
   )
 }
