72744ba906a6256c409a9a9f0c034faeb691707f
Alexia A-J <alexia_allard-jacquin@hotmail.fr>
2024-02-28 08:25:27 +0100

    Add Footer and styling + responsive

--- src/components/Footer/index.jsx
+++ src/components/Footer/index.jsx
@@ -1,5 +1,14 @@
+import FooterLogo from '../../assets/logo-footer.svg'
+
 function Footer() {
-    return
+    return (
+        <footer className='footerWrapper'>
+            <figure className='footerWrapper__fig'>
+                <img src={FooterLogo} alt='Logo de Kasa' />
+            </figure>
+            <p className='footerWrapper__copyright'>© 2020 Kasa. All rights reserved</p>
+        </footer>
+    )
 }
 
 export default Footer
