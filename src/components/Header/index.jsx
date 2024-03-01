6db1171420700266ad4da8bdc45f11ae77c1ba11
Alexia A-J <alexia_allard-jacquin@hotmail.fr>
2024-03-01 12:36:01 +0100

    Add logo click redirection to homepage

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,11 +1,14 @@
 import Logo from '../../assets/LOGO.svg'
 import Nav from '../Nav'
+import { Link } from 'react-router-dom'
 
 function Header() {
     return (
         <header className='headerWrapper'>
             <figure className='headerWrapper__fig'>
-                <img src={Logo} alt='Logo de Kasa' />
+            <Link to="/">
+                    <img src={Logo} alt='Logo de Kasa' />
+                </Link>
             </figure>
             <Nav />
         </header>
