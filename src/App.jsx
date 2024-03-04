0d46477e7a68f417e70e4169a1f21455d97bc165
Alexia A-J <alexia_allard-jacquin@hotmail.fr>
2024-03-04 18:01:44 +0100

    Fix logement id link

--- src/App.jsx
+++ src/App.jsx
@@ -13,7 +13,7 @@ function App() {
       <Routes>
         <Route path='/' element={<Home />} />
         <Route path='/about' element={<About />} />
-        <Route path='/logement/id' element={<FicheLogement />} />
+        <Route path='/logement/:id' element={<FicheLogement />} />
         <Route path='*' element={<Error />} />
       </Routes>
       <Footer />
