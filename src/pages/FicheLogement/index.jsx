0d2c783d47b2aefd7e109e9ee62d599f9e90a521
Alexia A-J <alexia_allard-jacquin@hotmail.fr>
2024-03-05 10:45:44 +0100

    Add use of id parameter + error nav + collapse elements

--- src/pages/FicheLogement/index.jsx
+++ src/pages/FicheLogement/index.jsx
@@ -1,8 +1,22 @@
+import logementsData from '../../datas/logements.json'
 import Collapse from '../../components/Collapse'
+import { useState, useEffect } from 'react'
+import { useParams, useNavigate } from 'react-router-dom'
 
 function FicheLogement() {
+
+    const { id } = useParams();
+    const logementData = logementsData.find(data => data.id === id)
+    const navigate = useNavigate()
+
+    useEffect(() => {
+        if (!logementData) {
+            navigate('*')
+        }
+    }, [id, logementData, navigate])
+
     return (
-        <div className="contentWrapper">
+        logementData && <div className="contentWrapper">
             {/* SLIDESHOW ?? */}
             <div>
                 <h1>
@@ -19,9 +33,25 @@ function FicheLogement() {
                 {/* RATING COMPONENT ?? */}
                 {/* AUTEUR COMPONENT ?? */}
             </div>
-            <div>
-                <Collapse />
-                <Collapse />
+            <div className='collapsesWrapper'>
+                <div className='collapses collapseDescription'>
+                    <Collapse 
+                    title='Description'
+                    text={logementData.description}
+                    />
+                </div>
+                <div className='collapses'>
+                    <Collapse 
+                    title='Equipements'
+                    text={
+                        <ul>
+                            {logementData.equipments.map((equipment, index) => (
+                                <li key={index}>{equipment}</li>
+                            ))}
+                        </ul>
+                    }
+                    />
+                </div>
             </div>
         </div>
     )
