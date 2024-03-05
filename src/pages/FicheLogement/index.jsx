3f266d26872c9c0ff94b8222a22dc75b1227a010
Alexia A-J <alexia_allard-jacquin@hotmail.fr>
2024-03-05 09:21:36 +0100

    Outline FicheLogement structure

--- src/pages/FicheLogement/index.jsx
+++ src/pages/FicheLogement/index.jsx
@@ -1,5 +1,30 @@
+import Collapse from '../../components/Collapse'
+
 function FicheLogement() {
-    return
+    return (
+        <div className="contentWrapper">
+            {/* SLIDESHOW ?? */}
+            <div>
+                <h1>
+
+                </h1>
+                <h2>
+
+                </h2>
+                <div>
+                    {/* TAG COMPONENT ?? */}
+                </div>
+            </div>
+            <div>
+                {/* RATING COMPONENT ?? */}
+                {/* AUTEUR COMPONENT ?? */}
+            </div>
+            <div>
+                <Collapse />
+                <Collapse />
+            </div>
+        </div>
+    )
 }
 
 export default FicheLogement
